fe18c000a5d268dca58c774caabb82076c8d534d
Kaif-Maredia <kaif.k.maredia@gmail.com>
2025-03-16 22:52:58 -0500

    Added Add Customer

--- crm-app/src/App.vue
+++ crm-app/src/App.vue
@@ -13,6 +13,7 @@ import HelloWorld from './components/HelloWorld.vue'
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/AddCustomer">Add Customer</RouterLink>
       </nav>
     </div>
   </header>
